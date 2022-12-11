7b5982db89177bce5e5a1fe514915e32ab30bd12
joalor93 <114845896+joalor93@users.noreply.github.com>
2022-12-11 04:45:02 -0300

    Add files via upload

---
+++ model_fitting/model_fitting_kappa_05.R
@@ -0,0 +1,37 @@
+library(OBASpatial)
+set.seed(25)
+data(dataca20)
+
+##training data ### 
+d1=dataca20[1:158,]
+
+
+########################
+##### priors############
+########################
+#
+#### kappa = 0.5#######
+
+##### reference
+res=tsroba(calcont~altitude+area, kappa = 0.5, data=d1,
+          ini.pars=c(10,200,10),iter=11000,burn=1000,thin=10)
+
+
+##### jeffreys 
+resjef=tsroba(calcont~altitude+area, kappa = 0.5, data=d1,
+           ini.pars=c(10,200,10),iter=11000,burn=1000,thin=10,
+           prior = 'jef.rul')
+
+### independent jeffreys 
+resjefind=tsroba(calcont~altitude+area, kappa = 0.5, data=d1,
+           ini.pars=c(10,200,10),iter=11000,burn=1000,thin=10,
+           prior = 'jef.ind')
+
+
+
+#### vague 
+resvague=tsroba(calcont~altitude+area, kappa = 0.5, data=d1,
+                 ini.pars=c(10,200,10),iter=11000,burn=1000,thin=10,
+                 prior = 'vague')
+
+
---
+++ model_fitting/model_fitting_kappa_07.R
@@ -0,0 +1,35 @@
+library(OBASpatial)
+set.seed(25)
+data(dataca20)
+
+##training data ### 
+d1=dataca20[1:158,]
+
+########################
+##### priors############
+########################
+#
+#### kappa = 0.5#######
+
+##### reference
+res=tsroba(calcont~altitude+area, kappa = 0.7, data=d1,
+           ini.pars=c(10,230,10),iter=11000,burn=1000,thin=10)
+
+##### jeffreys 
+resjef=tsroba(calcont~altitude+area, kappa = 0.7, data=d1,
+              ini.pars=c(10,230,10),iter=11000,burn=1000,thin=10,
+              prior = 'jef.rul')
+
+
+### independent jeffreys 
+resjefind=tsroba(calcont~altitude+area, kappa = 0.7, data=d1,
+                 ini.pars=c(10,230,10),iter=11000,burn=1000,thin=10,cprop =2,
+                 prior = 'jef.ind')
+
+
+#### vague 
+resvague=tsroba(calcont~altitude+area, kappa = 0.7, data=d1,
+                ini.pars=c(10,230,10),iter=11000,burn=1000,thin=10,cprop =2,
+                prior = 'vague')
+
+
